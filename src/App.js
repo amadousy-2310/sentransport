02ce6b09812d35b8c814ffed7e381dd1ff7b5854
amadousy-2310 <amadoudembasy@esp.sn>
2026-04-12 14:27:04 +0000

    Setup initial SenTransport avec Header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="contenu">
+        <h1>Bienvenue sur SénTransport</h1>
+        <p>Le transport en commun à Dakar</p>
+      </div>
     </div>
   );
 }
